e1d80d8074ea9291108bebadec8778929972c7ec
sfcodes22 <89711600+sfcodes22@users.noreply.github.com>
2021-10-08 19:50:40 -0400

    Update Word.java
    Changes to comments.

--- Week5/src/Word_Search/Word.java
+++ Week5/src/Word_Search/Word.java
@@ -1,13 +1,13 @@
 /*
  * File: Word.java
  * Author: Sherry Funches
- * Revision Date: October 3, 2021
+ * Revision Date: October 8, 2021
  * Purpose: Class creates a word object for the word search game. Words hold
  * the text of words hidden in the puzzle and the letters(JLabels) for each word.
  */
 
-
 package Word_Search;
+
 import java.awt.Color;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,19 +15,22 @@ import javax.swing.JLabel;
 
 
 public class Word {
-	private String text;
-	private Set <JLabel>wordLetters;
+	private String text; 
+	//individual letters that make up a word
+	//used to display word on word search grid
+	private Set <JLabel>wordLetters; 
 	
 	public Word(String text) {
 		this.text = text;
 		wordLetters = new HashSet<JLabel>();
 	}
 	
+	//adds a letter to the word's set of letters
 	public void addLetter(JLabel letter) {
 		wordLetters.add(letter);
 	}
 	
-	//Function used to check if the letters intersected by a highlight/line
+	//Function is used to check if the letters intersected by a highlight/line
 	//are the same as those in a word
 	public boolean isEqual (Set <JLabel> lineLetters) {
 		return wordLetters.equals(lineLetters);
@@ -36,6 +39,7 @@ public class Word {
 	public String toString() {
 		return  text;
 	}
+	
 	//Used to change color of words that are not found by the user during scoring.
 	public void changeAllLetterColor(Color color) {
 		for (JLabel letter: wordLetters) {
